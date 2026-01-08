687e6cab646fcc394f296493f4a28af361177bf1
mccoderdev <230913753+mccoderdev@users.noreply.github.com>
2026-01-07 20:09:09 -0800

    Update BaseTele.kt with the new changes (more changes)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition_Code/Tele/OpModes/BlueTele.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition_Code/Tele/OpModes/BlueTele.kt
@@ -4,11 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.Competition_Code.AllianceColor
 
-@TeleOp(name = "NewBlueTele", group = "Linear OpMode")
+@TeleOp(name = "BlueTele", group = "Linear OpMode")
 class BlueTele : LinearOpMode() {
     override fun runOpMode() {
         // Create the instance of our BaseTele
-        val baseTele: BaseTele = BaseTele(this, AllianceColor.Blue)
+        val baseTele = BaseTele(this, AllianceColor.Blue)
 
         // There is no setup code to run here because it is all ran
         // in BaseTele!
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition_Code/Tele/OpModes/RedTele.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition_Code/Tele/OpModes/RedTele.kt
@@ -4,11 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.Competition_Code.AllianceColor
 
-@TeleOp(name = "NewRedTele", group = "Linear OpMode")
+@TeleOp(name = "RedTele", group = "Linear OpMode")
 class RedTele : LinearOpMode() {
     override fun runOpMode() {
         // Create the instance of our BaseTele
-        val baseTele: BaseTele = BaseTele(this, AllianceColor.Red)
+        val baseTele = BaseTele(this, AllianceColor.Red)
 
         // There is no setup code to run here because it is all ran
         // in BaseTele!
