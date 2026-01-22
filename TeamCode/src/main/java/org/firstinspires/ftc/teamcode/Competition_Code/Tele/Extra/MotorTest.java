5265f39a7fc494bf4bc29f31f8b923293bdb61d3
burntorangepeel1 <230786482+burntorangepeel1@users.noreply.github.com>
2026-01-21 19:34:58 -0800

    created a motor test to increase/decrease power from gamepad

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition_Code/Tele/Extra/MotorTest.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Competition_Code.Tele.Extra;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.*;
+public class MotorTest {
+    public DcMotorEx launcher;
+    public double power = 0;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition_Code/Tele/Extra/SingleMotorTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Competition_Code.Tele.Extra;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "SingleMotorTest")
+public class SingleMotorTest extends LinearOpMode {
+    DcMotorEx motor;
+    double power = 0;
+    @Override
+    public void runOpMode() {
+        ElapsedTime timer = new ElapsedTime();
+
+        motor = hardwareMap.get(DcMotorEx.class, "motor");
+        waitForStart();
+        while (opModeIsActive()) {
+            if(gamepad1.dpad_up && timer.milliseconds() > 60){
+                power += 0.05;
+                timer.reset();
+            }
+            else if(gamepad1.dpad_down  && timer.milliseconds() > 60){
+                power -= 0.05;
+                timer.reset();
+            }
+            if (power < 0){
+                power = 0;
+            }
+            else if (power > 1){
+                power = 1;
+            }
+            motor.setPower(power);
+        }
+    }
+}
